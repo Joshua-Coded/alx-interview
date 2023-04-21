65ce794a8be84929b873f7dac0b36852d59208a4
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-20 19:11:45 -0700

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return list()
+
     empty_list = list([1])
+
     for i in range(1, n):
-        row = [1]
+        row = list([1])
+
         for j in range(1, i):
-            row.append(empty_list[i-1] + empty_list[i-1][j])
-            row.append(i)
-            empty_list.append(row)
-        return empty_list
+            row.append(empty_list[i-1][j-1] + empty_list[i-1][j])
+        row.append(1)
+        empty_list.append(row)
+
+    return empty_list
