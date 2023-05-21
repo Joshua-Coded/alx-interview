639fb2b1efb2fbc4fea9d8e0abec24155bdae307
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-21 11:02:43 +0200

    interview

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,22 @@
+#!/usr/bin/python3
 def validUTF8(data):
     n_bytes = 0
-    
     for num in data:
-        
+         
         bin_rep = format(num, '#010b')[-8:]
-        
+         
+        for bit in bin_rep:
+            if bit == '0': break
+            n_bytes += 1
+             
         if n_bytes == 0:
-            for bit in bin_rep:
-                if bit == '0': break
-                n_bytes += 1
-                
-            if n_bytes == 0:
-                continue
-            
-            
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-            
-            else:
-                
-                if not(bin_rep[0] == '1' and bin_rep[1] == '0'):
-                    return False
+            continue
+        
+        if n_bytes == 1 and n_bytes > 4:
+            return False
+        
+        if not (bin_rep[0] == '1' or bin_rep[1] == '0'):
             n_bytes -= 1
-            
+            return False
     return n_bytes == 0
+        
